edaeaa4f069f3c4611e0b6356df39725bde998e4
nethmag <ssnethmagallage@gmail.com>
2021-04-23 22:55:15 +0530

    Adding lottie animation to the project

---
+++ app/src/main/java/gloginexample/example/moremeals/MainActivity.java
@@ -0,0 +1,20 @@
+package gloginexample.example.moremeals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide(); //hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
+    }
+}
